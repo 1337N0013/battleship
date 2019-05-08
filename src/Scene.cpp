cd29c1a77299cf02b492d1f55d431a0a116ed52a
1337N0013 <grantivanyap@yahoo.com>
2019-05-08 13:50:41 +0800

    created Scene.h Scene.cpp

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,1 +1,13 @@
 #include "Scene.h"
+
+Scene::Scene(sf::RenderWindow& window) : mWindow(window) {
+
+}
+
+void Scene::start() {
+
+}
+
+void Scene::input(sf::Event e) {
+
+}
--- src/Scene.h
+++ src/Scene.h
@@ -1,8 +1,18 @@
+#include <SFML/Graphics.hpp>
+
 #ifndef SCENE_H
 #define SCENE_H
 
 class Scene {
-    
+   public:
+    Scene(sf::RenderWindow& window);
+    virtual void start();
+    virtual void input(sf::Event e);
+    virtual void draw();
+    virtual void update();
+
+   private:
+    sf::RenderWindow& mWindow;
 };
 
 #endif
