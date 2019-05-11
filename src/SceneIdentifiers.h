5185ecd4ea76b30fde57f0f4d895c83cd8805039
Grant Yap <grantivanyap@yahoo.com>
2019-05-11 13:45:10 +0800

    replace Scenes.h with SceneIdentifiers.h

---
+++ src/SceneIdentifiers.h
@@ -0,0 +1,14 @@
+#ifndef SCENEIDENTIFIERS_H
+#define SCENEIDENTIFIERS_H
+
+#include "MainMenuScene.h"
+#include "Scene.h"
+#include <unordered_map>
+
+namespace Scenes {
+    enum class ID {
+        MainMenu
+    };
+}
+
+#endif
