3163dd43762474fbde696e082771e6846239d920
Grant Yap <grantivanyap@yahoo.com>
2019-05-11 22:52:39 +0800

    SceneIdentifiers.h: removed useless #include statements

--- src/SceneIdentifiers.h
+++ src/SceneIdentifiers.h
@@ -1,10 +1,6 @@
 #ifndef SCENEIDENTIFIERS_H
 #define SCENEIDENTIFIERS_H
 
-#include <unordered_map>
-#include "MainMenuScene.h"
-#include "Scene.h"
-
 namespace Scenes {
 enum class ID { MainMenu };
 }
