9f9ab4f88ddbb9b04ca069bce8992ad1a5677ada
Grant Yap <grantivanyap@yahoo.com>
2019-05-21 23:02:45 +0800

    Fix BoardCell::draw() definition and declaration

--- src/BoardCell.cpp
+++ src/BoardCell.cpp
@@ -2,6 +2,6 @@
 
 BoardCell::BoardCell(Scene::Context& context) : Button("", context.font) {}
 
-void BoardCell::draw(sf::RenderTarget& target, sf::RenderStates states) {
+void BoardCell::draw(sf::RenderTarget& target, sf::RenderStates states) const {
     Button::draw(target, states);
 }
