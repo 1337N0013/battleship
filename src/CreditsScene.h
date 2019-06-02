6f0b0ca81620a766c84fea0e2a43e14a9b2f5bd1
Grant Yap <grantivanyap@yahoo.com>
2019-06-02 13:46:47 +0800

    Implement CreditsScene

---
+++ src/CreditsScene.h
@@ -0,0 +1,25 @@
+#ifndef CREDITSSCENE_H
+#define CREDITSSCENE_H
+
+#include "Scene.h"
+#include "Button.h"
+
+class CreditsScene : public Scene {
+    public:
+    CreditsScene(SceneStack& stack, Context context);
+    ~CreditsScene();
+
+    bool input(const sf::Event& e);
+    void draw();
+    bool update(sf::Time deltatTime);
+
+    private:
+    sf::RenderWindow& mWindow;
+
+    sf::RectangleShape mBackground;
+    sf::Text mCredits;
+    sf::Text mCreditsList;
+    Button mReturnToMainMenu;
+};
+
+#endif
